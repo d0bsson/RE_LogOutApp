2547a8f6e48ede8b39e72cc76951cedc072fc912
David <ber.dev@yandex.ru>
2021-08-31 14:33:48 +0300

    Initial Commit

---
+++ RE_LogOutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RE_LogOutApp
+//
+//  Created by Дэвид Бердников on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
