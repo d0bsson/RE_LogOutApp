0594f82fad8bfcfe97c64bc3d6676f04c691107d
David <ber.dev@yandex.ru>
2021-09-01 00:19:50 +0300

    i know what is it clouser

---
+++ RE_LogOutApp/LogOutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LogOutViewController.swift
+//  RE_LogOutApp
+//
+//  Created by Дэвид Бердников on 31.08.2021.
+//
+
+import UIKit
+
+class LogOutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- RE_LogOutApp/ViewController.swift
+++ RE_LogOutApp/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let login = "login"
+    let password = "123"
+    
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var forgottenLoginButton: UIButton!
+    @IBOutlet weak var forgottenPassButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func logInTapped() {
+        
+    }
+    
+    
+    @IBAction func forgottenLoginTapped() {
+        alert(title: "You login is \(login)")
+    }
+    
+    
+    @IBAction func forgottenPassTapped() {
+        alert(title: "You password is \(password)")
+    }
+    
+    private func alert(title: String) {
+        let alert = UIAlertController()
+        let okAction = UIAlertAction(title: title, style: .default) { _ in
+            self.passwordTF.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
